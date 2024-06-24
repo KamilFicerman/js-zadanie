fbf6a3b3ecf7897ca5c32d3d009e72d78d7c71d9
KamilFicerman <154465288+KamilFicerman@users.noreply.github.com>
2024-06-24 18:51:24 +0200

    add first very simple version of app, add proxy to bypass cors

--- index.js
+++ index.js
@@ -1,6 +1,42 @@
-const header = document.getElementById('header');
-document.addEventListener('click', test);
+let images = [];
+const output = document.getElementById('output');
 
-function test(){
-    header.textContent = `It works!`;
+document.getElementById('fileInput').addEventListener('change', function(event) {
+    const file = event.target.files[0];
+    if (!file) {
+        return;
+    }
+
+    const reader = new FileReader();
+    reader.onload = function(e) {
+        hm(e);
+    };
+    reader.readAsText(file);
+});
+
+async function hm(e){
+    const contents = e.target.result;
+        const lines = contents.split('\n');
+        const outputDiv = document.getElementById('output');
+        outputDiv.innerHTML = ''; // Clear previous content
+
+        lines.forEach(line => {
+            // Trim any leading/trailing whitespace and skip empty lines
+            const trimmedLine = line.trim();
+            if (trimmedLine) {
+                images.push(Number(trimmedLine));
+            }
+        });
+
+        images.forEach(async num => await getData(num))
+}
+
+async function getData(num){
+    images.forEach(async num => {
+        let response = await fetch(`https://xkcd-api.ridvanaltun.vercel.app/api/comics/${num}`);
+        let comicsObject = response.json();
+        let img = document.createElement('img');
+        img.src = comicsObject.img;
+        output.append(img);
+});
 }
